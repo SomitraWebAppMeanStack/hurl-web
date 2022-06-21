167a2b2db206814f29fdbb304cc07ee022927acd
somitra <somitra@webapp.world>
2022-06-21 14:50:43 +0530

    added build files

---
+++ build/precache-manifest.54abc1a90839129cb1e2b2b825c91b5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10e309daefbeefabe26a",
+    "url": "/static/css/main.16ac40eb.chunk.css"
+  },
+  {
+    "revision": "10e309daefbeefabe26a",
+    "url": "/static/js/main.10e309da.chunk.js"
+  },
+  {
+    "revision": "3feb7ea82aa44a243ec8",
+    "url": "/static/js/1.3feb7ea8.chunk.js"
+  },
+  {
+    "revision": "a93a3f691253d0927f11",
+    "url": "/static/css/2.30bdec65.chunk.css"
+  },
+  {
+    "revision": "a93a3f691253d0927f11",
+    "url": "/static/js/2.a93a3f69.chunk.js"
+  },
+  {
+    "revision": "745e60049166c645a4b1",
+    "url": "/static/js/runtime~main.745e6004.js"
+  },
+  {
+    "revision": "61212cec3ba98a069a56436964b7d6ce",
+    "url": "/index.html"
+  }
+];
